665ef13cc3cb0ec5b21f960cb961999f92430341
dancorrea86 <danielmacielcorrea@gmail.com>
2019-10-26 16:58:09 -0300

    commit

--- server/controllers/.js
+++ server/controllers/.js
@@ -13,14 +13,3 @@ module.exports = {
   },
 }
 
-  getAll: function (req, res) {
-    Task.find(function(err, tasks) {
-      if (err) {
-        res.status(400).send("There's was an error while retrieving the tasks");s
-      } 
-      else {
-        res.status(200).json(tasks);
-      }
-    })
-  }
-}
